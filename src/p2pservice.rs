8fcdd47782c2c43d5ad65355f5e3849b67bd567f
Gavin Hurlbut <gjhurlbu@gmail.com>
2021-10-09 23:23:48 -0700

    Initial version
    - experimenting with RPC for P2P connection
    - currently targeting CBOR with tarpc

---
+++ src/p2pservice.rs
@@ -0,0 +1,39 @@
+// Copyright 2018 Google LLC
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+use serde_derive::{Deserialize, Serialize};
+//use std::error::Error;
+//use std::fs::File;
+use std::net::IpAddr;
+use std::env;
+use tracing_subscriber::{fmt::format::FmtSpan, prelude::*};
+use std::collections::HashMap;
+
+/// This is the service definition. It looks a lot like a trait definition.
+/// It defines one RPC, hello, which takes one arg, name, and returns a String.
+#[tarpc::service]
+pub trait P2PService {
+    async fn version(request: HashMap<String, String>) -> HashMap<String, String>;
+}
+
+/// Initializes an OpenTelemetry tracing subscriber with a Jaeger backend.
+pub fn init_tracing(service_name: &str) -> anyhow::Result<()> {
+    env::set_var("OTEL_BSP_MAX_EXPORT_BATCH_SIZE", "12");
+
+    let tracer = opentelemetry_jaeger::new_pipeline()
+        .with_service_name(service_name)
+        .with_max_packet_size(2usize.pow(13))
+        .install_batch(opentelemetry::runtime::Tokio)?;
+
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::EnvFilter::from_default_env())
+        .with(tracing_subscriber::fmt::layer().with_span_events(FmtSpan::NEW | FmtSpan::CLOSE))
+        .with(tracing_opentelemetry::layer().with_tracer(tracer))
+        .try_init()?;
+
+    Ok(())
+}
+
